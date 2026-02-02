236b52a9cdf600f95e7fcd40bbab3b2b0d09e791
dilakh031-crypto <dilakh031@gmail.com>
2026-02-02 05:27:12 +0100

    Add files via upload

--- app/src/main/java/is/xyz/mpv/MPVActivity.kt
+++ app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -2342,7 +2342,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
 
             if (ratio <= 0.01)
                 return ProbedOrientation.UNKNOWN
-            if (ratio in (1.0 / ASPECT_RATIO_MIN) .. ASPECT_RATIO_MIN)
+            if (ratio in (1.0 / ASPECT_RATIO_MIN.toDouble()) .. ASPECT_RATIO_MIN.toDouble())
                 return ProbedOrientation.SQUARE
             return if (ratio > 1.0) ProbedOrientation.LANDSCAPE else ProbedOrientation.PORTRAIT
         } catch (_: Throwable) {
@@ -2443,7 +2443,7 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         return ProbedOrientation.UNKNOWN
     }
 
-    private fun applyOrientationFromMetadata(path: String, isStartup: Boolean = false) {
+    private fun applyOrientationFromMetadata(path: String, isStartup: Boolean = false) {(path: String, isStartup: Boolean = false) {
         // screen orientation is fixed (Android TV)
         if (!packageManager.hasSystemFeature(PackageManager.FEATURE_SCREEN_PORTRAIT))
             return
