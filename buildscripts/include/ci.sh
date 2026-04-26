ce82acc0891a9ed708a4809f9d6789cbffa81f0e
dilakh031-crypto <dilakh031@gmail.com>
2026-04-26 03:25:39 +0100

    Add files via upload

--- buildscripts/include/ci.sh
+++ buildscripts/include/ci.sh
@@ -3,7 +3,14 @@
 # go to buildscripts root folder
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
+# CI should build arm64 (arm64-v8a) by default.
+# You can override this by setting ARCH (armv7l, arm64, x86, x86_64).
+ARCH="${ARCH:-arm64}"
+
 . ./include/depinfo.sh
+
+# Make cache identifiers architecture-specific to avoid mixing prefixes.
+ci_tarball="${ci_tarball%.tgz}-arch-${ARCH}.tgz"
 
 msg() {
 	printf '==> %s\n' "$1"
@@ -32,7 +39,7 @@ build_prefix() {
 	IN_CI=1 ./include/download-deps.sh
 
 	msg "Compiling"
-	./buildall.sh --only-deps mpv
+	./buildall.sh --arch "$ARCH" --only-deps mpv
 
 	if [[ "$CACHE_MODE" == folder && -w "$CACHE_FOLDER" ]]; then
 		msg "Compressing the prefix"
@@ -76,13 +83,13 @@ else
 fi
 
 msg "Building mpv"
-./buildall.sh -n mpv || {
+./buildall.sh --arch "$ARCH" -n mpv || {
 	# show logfile if configure failed
 	[ ! -f deps/mpv/_build/config.h ] && cat deps/mpv/_build/meson-logs/meson-log.txt
 	exit 1
 }
 
 msg "Building mpv-android"
-./buildall.sh -n
+./buildall.sh --arch "$ARCH" -n
 
 exit 0
