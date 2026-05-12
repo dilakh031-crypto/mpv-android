d6f3c2ef437cd55bfd6f77c873dec4dd182f15c5
dilakh031-crypto <dilakh031@gmail.com>
2026-05-12 05:33:45 +0100

    Add files via upload

--- app/src/main/jni/render.cpp
+++ app/src/main/jni/render.cpp
@@ -16,18 +16,13 @@ static jobject surface;
 jni_func(void, attachSurface, jobject surface_) {
     CHECK_MPV_INIT();
 
-    jobject old_surface = surface;
     surface = env->NewGlobalRef(surface_);
     if (!surface)
         die("invalid surface provided");
-
     int64_t wid = reinterpret_cast<intptr_t>(surface);
     int result = mpv_set_option(g_mpv, "wid", MPV_FORMAT_INT64, &wid);
     if (result < 0)
          ALOGE("mpv_set_option(wid) returned error %s", mpv_error_string(result));
-
-    if (old_surface)
-        env->DeleteGlobalRef(old_surface);
 }
 
 jni_func(void, detachSurface) {
@@ -38,8 +33,6 @@ jni_func(void, detachSurface) {
     if (result < 0)
          ALOGE("mpv_set_option(wid) returned error %s", mpv_error_string(result));
 
-    if (surface) {
-        env->DeleteGlobalRef(surface);
-        surface = NULL;
-    }
+    env->DeleteGlobalRef(surface);
+    surface = NULL;
 }
