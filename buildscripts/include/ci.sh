db5f4fabfe927d10ba217bd76884cfac6e51a422
dilakh031-crypto <dilakh031@gmail.com>
2026-05-18 23:12:49 +0100

    Add files via upload

--- buildscripts/include/ci.sh
+++ buildscripts/include/ci.sh
@@ -5,6 +5,13 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 . ./include/depinfo.sh
 
+ci_arch=${CI_ARCH:-armv7l}
+if [ -n "${CI_ARCH:-}" ]; then
+	ci_cache_identifier="${ci_tarball%.tgz}-${ci_arch}.tgz"
+else
+	ci_cache_identifier="$ci_tarball"
+fi
+
 msg() {
 	printf '==> %s\n' "$1"
 }
@@ -17,8 +24,8 @@ fetch_prefix() {
 		else
 			echo "Cache seems to be empty"
 		fi
-		printf 'Expecting "%s",\nfound     "%s".\n' "$ci_tarball" "$text"
-		if [[ "$text" == "$ci_tarball" ]]; then
+		printf 'Expecting "%s",\nfound     "%s".\n' "$ci_cache_identifier" "$text"
+		if [[ "$text" == "$ci_cache_identifier" ]]; then
 			tar -xzf "$CACHE_FOLDER/data.tgz" -C prefix && return 0
 		fi
 	fi
@@ -32,12 +39,12 @@ build_prefix() {
 	IN_CI=1 ./include/download-deps.sh
 
 	msg "Compiling"
-	./buildall.sh --only-deps mpv
+	./buildall.sh --arch "$ci_arch" --only-deps mpv
 
 	if [[ "$CACHE_MODE" == folder && -w "$CACHE_FOLDER" ]]; then
 		msg "Compressing the prefix"
 		tar -cvzf "$CACHE_FOLDER/data.tgz" -C prefix .
-		echo "$ci_tarball" >"$CACHE_FOLDER/id.txt"
+		echo "$ci_cache_identifier" >"$CACHE_FOLDER/id.txt"
 	fi
 }
 
@@ -45,7 +52,7 @@ export WGET="wget --progress=bar:force"
 
 if [ "$1" = "export" ]; then
 	# export variable with unique cache identifier
-	echo "CACHE_IDENTIFIER=$ci_tarball"
+	echo "CACHE_IDENTIFIER=$ci_cache_identifier"
 	exit 0
 elif [ "$1" = "install" ]; then
 	# install deps
@@ -76,13 +83,13 @@ else
 fi
 
 msg "Building mpv"
-./buildall.sh -n mpv || {
+./buildall.sh --arch "$ci_arch" -n mpv || {
 	# show logfile if configure failed
 	[ ! -f deps/mpv/_build/config.h ] && cat deps/mpv/_build/meson-logs/meson-log.txt
 	exit 1
 }
 
 msg "Building mpv-android"
-./buildall.sh -n
+./buildall.sh --arch "$ci_arch" -n
 
 exit 0
